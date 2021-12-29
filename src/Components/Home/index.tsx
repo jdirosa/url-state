b3c275bbbd9f3186b68cac38c8b704cdc64fbb93
James DiRosa <jdirosa@barracuda.com>
2021-12-28 22:34:53 -0500

    init commit

---
+++ src/Components/Home/index.tsx
@@ -0,0 +1,46 @@
+import {
+	Dialog,
+	DialogTitle,
+	DialogContent,
+	DialogActions,
+	Button,
+} from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+import { useUrlState } from "../../url-state/useUrlState";
+
+interface User {
+	name: string;
+	id: string;
+}
+
+export const Home = () => {
+	const defaultUser: User = {
+		name: "james",
+		id: "1234",
+	};
+	const [open, setOpen] = useUrlState("open", false);
+	const [user, setUser] = useUrlState("user", defaultUser);
+
+	return (
+		<>
+			<div className="App">
+				<header className="App-header">
+					<p>Name: {user?.name}</p>
+					<p>Id: {user?.id}</p>
+					<Link to="/2">Home</Link>
+					<Button onClick={() => setOpen(true)}>Show Dialog</Button>
+				</header>
+			</div>
+			<Dialog maxWidth="sm" open={open}>
+				<DialogTitle>Title!</DialogTitle>
+				<DialogContent style={{ width: "100%" }}>
+					This is a test one
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={() => setOpen(false)}>Close</Button>
+				</DialogActions>
+			</Dialog>
+		</>
+	);
+};
---
+++ src/Components/OtherHome/index.tsx
@@ -0,0 +1,43 @@
+import {
+	Dialog,
+	DialogTitle,
+	DialogContent,
+	DialogActions,
+	Button,
+} from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+import { useUrlState } from "../../url-state/useUrlState";
+
+interface User {
+	name: string;
+	id: string;
+}
+
+export const OtherHome = () => {
+	const defaultUser: User = {
+		name: "james",
+		id: "1234",
+	};
+	const [open, setOpen] = useUrlState("open2", false);
+	const [user, setUser] = useUrlState("user2", defaultUser);
+
+	return (
+		<>
+			<div className="App">
+				<header className="App-header">
+					<p>Name: {user?.name}</p>
+					<p>Id: {user?.id}</p>
+					<Link to="/">Home</Link>
+				</header>
+			</div>
+			<Dialog open={open}>
+				<DialogTitle>Title!</DialogTitle>
+				<DialogContent>This is a test one</DialogContent>
+				<DialogActions>
+					<Button onClick={() => setOpen(false)}>Close</Button>
+				</DialogActions>
+			</Dialog>
+		</>
+	);
+};
